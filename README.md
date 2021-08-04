e4cf2bc66d5edfcbb6c8fe5b55a5cb9acaeb7f73
BenKurrek <bkurrek@uwaterloo.ca>
2021-08-04 18:35:40 -0400

    Basic implementation sudo finished. Can track incoming and outcoming flow of funds for a given contract

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
-# Fayyr Indexer 
+# Bookkeeping Indexer 
 
 ## 🚨🚨🚨 WORK IN PROGRESS 🚨🚨🚨
 
-An indexer that catches specific method calls for specific contracts and relays information to a PostgreSQL database. Created by: 
+An indexer that keeps track of all incoming and outgoing funds for a specific contract 
 - @BenKurrek - https://github.com/BenKurrek
-- @mariavmihu - https://github.com/mariavmihu
-- @frol - https://github.com/frol
 - @khorolets - https://github.com/khorolets
 
 ## TODO / DONE:
 - [x] basic indexer working
-- [x] match statements to determine which method has been called
-- [x] struct for passing information to database
+- [x] check if funds are coming in or out
+- [x] struct for ease of access for information
 - [x] cross-contract calls
-- [ ] connect to database to do posts
-- [ ] do we want to keep track of unsuccessful transactions in a DB? 
+- [ ] connect to excel or csv
 
 ## Running Indexer:
 - install dependencies and compile code using `cargo check`
@@ -25,25 +22,28 @@ An indexer that catches specific method calls for specific contracts and relays 
 MAKE SURE TO DO THIS FIRST: set NEAR_ENV variable locally using: `export NEAR_ENV=localnet`
 
 View For Sale Listings
-- `near view market.test.near get_sales_by_nft_contract_id '{"nft_contract_id": "test.near", "from_index": "0", "limit": 50}'`
+- `near view market22.test.near get_sales_by_nft_contract_id '{"nft_contract_id": "test.near", "from_index": "0", "limit": 50}'`
 
 View Tokens On NFT Contract
 - `near view test.near nft_tokens '{"from_index": "0", "limit": 50}'`
 
 Update Price (make sure the token exists)
-- `near call --accountId ben.test.near market.test.near update_price '{"nft_contract_id": "test.near", "token_id": "3", "ft_token_id": "near", "price": "5"}' --amount 0.000000000000000000000001`
+- `near call --accountId ben.test.near market2.test.near update_price '{"nft_contract_id": "test.near", "token_id": "3", "ft_token_id": "near", "price": "5"}' --amount 0.000000000000000000000001`
 
 Remove Sale (make sure the token exists)
-- `near call --accountId ben.test.near market.test.near remove_sale '{"nft_contract_id": "test.near", "token_id": "3"}' --amount 0.000000000000000000000001`
+- `near call --accountId ben.test.near market2.test.near remove_sale '{"nft_contract_id": "test.near", "token_id": "3"}' --amount 0.000000000000000000000001`
 
 Place Item For Sale
-- `near call --accountId ben.test.near test.near nft_approve '{"token_id": "3", "account_id": "market.test.near", "msg": "{\"sale_conditions\":[{\"ft_token_id\":\"near\",\"price\":\"5000000000000000000000000\"}]}"}' --amount 1` 
+- `near call --accountId ben.test.near test.near nft_approve '{"token_id": "3", "account_id": "market2.test.near", "msg": "{\"sale_conditions\":[{\"ft_token_id\":\"near\",\"price\":\"5000000000000000000000000\"}]}"}' --amount 1` 
 
 Offer
-- `near call --accountId bob.test.near market.test.near offer '{"nft_contract_id": "test.near", "token_id": "3"}' --amount 5 --gas=200000000000000`
+- `near call --accountId bob.test.near market2.test.near offer '{"nft_contract_id": "test.near", "token_id": "3"}' --amount 5 --gas=200000000000000`
 
 Deposit Storage
-- `near call --accountId bob.test.near market.test.near storage_deposit '{}' --amount 0.1`
+- `near call --accountId bob.test.near market2.test.near storage_deposit '{}' --amount 0.1`
+
+Withdraw Storage
+- `near call --accountId ben.test.near market22.test.near storage_withdraw '{}' --amount 0.000000000000000000000001`
 
 ## Commands For NEAR CLI Account Stuff:
 Create New SubAccount
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use actix;
 
+use chrono::{NaiveDateTime, DateTime}; 
+use chrono::prelude::*; 
+
 use near_indexer::near_primitives::views::ExecutionStatusView;
-use near_indexer::near_primitives::types::BlockReference;
-use near_indexer::near_primitives::types::FunctionArgs;
-use near_indexer::near_primitives::views::QueryRequest;
-use near_client::Query;
 use near_client::ViewClientActor;
 
 use clap::Clap;
@@ -19,14 +18,19 @@ use serde::{Serialize, Deserialize};
 
 mod configs;
 
+static CONTRACT_TO_VIEW: &str = "market2.test.near"; 
+
 //use this struct to store information that we want to pass to database
 #[derive(Debug)] //derive debug so that we can print
 struct ExecutionDetails {
     method_name: String,
-    args: serde_json::Value,
+    predecessor_id: String,
+    receiver_id: String, 
     signer_id: String,
-    deposit: String,
+    function_call_deposit: String,
+    transfer_deposit: String,
     success_value: bool,
+    date: String, 
 }
 
 //declare struct for the return type of the blockchain view call
@@ -44,21 +48,34 @@ async fn listen_blocks(mut stream: mpsc::Receiver<near_indexer::StreamerMessage>
             //for each receipt and execution outcome pair in the shard
             for receipt_and_execution_outcome in shard.receipt_execution_outcomes {
                 // Check if receipt is related to Fayyr
-                if is_fayyr_receipt(&receipt_and_execution_outcome.receipt) {
+                if is_contract_receipt(&receipt_and_execution_outcome.receipt) {
                     //get the execution outcome from the receipt and execution outcome pair from the shard
                     let execution_outcome = receipt_and_execution_outcome.execution_outcome;
+                    let receipt = receipt_and_execution_outcome.receipt;
 
                     //declare the execution details that will hold specific wanted information
                     let mut execution_details = ExecutionDetails {
                         method_name: "".to_string(),
-                        args: serde_json::Value::String("".to_string()),
+                        predecessor_id: "".to_string(),
+                        receiver_id: "".to_string(),
                         signer_id: "".to_string(),
-                        deposit: "".to_string(),
+                        function_call_deposit: "".to_string(),
+                        transfer_deposit: "".to_string(),
                         success_value: matches!(execution_outcome.outcome.status, ExecutionStatusView::SuccessValue(_) | ExecutionStatusView::SuccessReceiptId(_)),
+                        date: "".to_string(),
                     };
-                    
+                    let nano_to_seconds = 1e+9 as u64; 
+                    let seconds = (streamer_message.block.header.timestamp / nano_to_seconds) as i64; 
+
+
+                    let naive = NaiveDateTime::from_timestamp(seconds, 0); 
+                    let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc); 
+                    let newdate = datetime.format("%Y-%m-%d %H:%M:%S");
+
+                    execution_details.date = newdate.to_string(); 
+
                     //get the signer id from the receipt
-                    let signer_id = if let near_indexer::near_primitives::views::ReceiptEnumView::Action { ref signer_id, .. } = receipt_and_execution_outcome.receipt.receipt {
+                    let signer_id = if let near_indexer::near_primitives::views::ReceiptEnumView::Action { ref signer_id, .. } = receipt.receipt {
                         Some(signer_id)
                     } else {
                         None
@@ -72,31 +89,44 @@ async fn listen_blocks(mut stream: mpsc::Receiver<near_indexer::StreamerMessage>
                         _ => {},
                     };
 
-                    //print the entire execution outcome for the current receipt
-                    eprintln!("Entire Execution Outcome ---> {:#?}", execution_outcome);   
+                    //get the predecessor_id from the receipt
+                    let predecessor_id = if let near_indexer::near_primitives::views::ReceiptView { ref predecessor_id, .. } = receipt {
+                        Some(predecessor_id)
+                    } else {
+                        None
+                    };
+
+                    //if there is some predecessor_id, set the execution detail's predecessor_id equal to it
+                    match predecessor_id {
+                        Some(predecessor_id) => {
+                            execution_details.predecessor_id = predecessor_id.to_string();
+                        },
+                        _ => {},
+                    };
+
+                    //get the receiver_id from the receipt
+                    let receiver_id = if let near_indexer::near_primitives::views::ReceiptView { ref receiver_id, .. } = receipt {
+                        Some(receiver_id)
+                    } else {
+                        None
+                    };
+
+                    //if there is some receiver_id, set the execution detail's receiver_id equal to it
+                    match receiver_id {
+                        Some(receiver_id) => {
+                            execution_details.receiver_id = receiver_id.to_string();
+                        },
+                        _ => {},
+                    };
 
                     //get the actions from the receipt
                     if let near_indexer::near_primitives::views::ReceiptEnumView::Action {
                         actions,
                         ..
-                    } = receipt_and_execution_outcome.receipt.receipt
+                    } = receipt.receipt
                     {   
                         //go through each action
                         for action in actions.iter() {
-                            //get the args from the action
-                            if let near_indexer::near_primitives::views::ActionView::FunctionCall {
-                                args,
-                                ..
-                            } = action
-                            {   
-                                //decode the args
-                                if let Ok(decoded_args) = base64::decode(args) {
-                                    //turn args into json and populate execution details
-                                    if let Ok(args_json) = serde_json::from_slice::<serde_json::Value>(&decoded_args) {
-                                        execution_details.args = args_json;
-                                    }
-                                }
-                            }
                             //get the method name 
                             if let near_indexer::near_primitives::views::ActionView::FunctionCall {
                                 method_name,
@@ -111,75 +141,38 @@ async fn listen_blocks(mut stream: mpsc::Receiver<near_indexer::StreamerMessage>
                                 ..
                             } = action
                             {
-                                execution_details.deposit = deposit.to_string();
+                                execution_details.function_call_deposit = deposit.to_string();
+                            }
+                            //get the transfer deposit if there is some
+                            if let near_indexer::near_primitives::views::ActionView::Transfer {
+                                deposit,
+                                ..
+                            } = action
+                            {
+                                execution_details.transfer_deposit = deposit.to_string();
                             }
                         }
                     }
 
-                    //only do stuff with the receipts if the outcome was successful
-                    if execution_details.success_value == true {
-                        //different cases based on the method that was called
-                        match execution_details.method_name.as_str() {
-                            //if remove_sale was called
-                            "remove_sale" => {
-                                eprintln!("Remove Sale Has Been Called")
-                            },
-                            //if update_price was called
-                            "update_price" => {
-                                eprintln!("Update Price Has Been Called")
-                            },
-                            //if offer was called
-                            "offer" => {
-                                eprintln!("Offer Has Been Called");
-
-                                //build the function arguments to get passed into nft_tokens_batch
-                                let function_args = serde_json::json!({
-                                    "token_ids": [execution_details.args.get("token_id").unwrap()],
-                                });
-                                
-                                //call nft_tokens_batch in order to get access to the current token owner
-                                let block_reference = BlockReference::latest();
-                                let request = QueryRequest::CallFunction {
-                                    //contract to call
-                                    account_id: "test.near".parse().unwrap(),
-                                    //method to view 
-                                    method_name: "nft_tokens_batch".to_string(),
-                                    //passed in arguments
-                                    args: FunctionArgs::from(function_args.to_string().into_bytes()),
-                                };
-                                let query = Query::new(block_reference, request);
-                                //get the response
-                                let response = view_client.send(query).await.unwrap().unwrap();
-
-                                if let near_indexer::near_primitives::views::QueryResponseKind::CallResult(call_result) = response.kind {
-                                    let result = call_result.result;
-
-                                    //set the output (of type vec<JsonToken>) equal to the result
-                                    let output: Vec<JsonToken> = serde_json::from_slice(&result).unwrap();
-                                    eprintln!("Current Token Owner {:?}", output[0].owner_id);
-
-                                    //check if the owner is the signer
-                                    if output[0].owner_id == execution_details.signer_id {
-                                        eprintln!("Signer Is Owner! Transaction Successful!");
-                                    } else {
-                                        eprintln!("Signer Is Not Owner... Transaction Failed.");
-                                    }
-                                }
-                            },
-                            //nft_on_approve was called
-                            "nft_on_approve" => {
-                                eprintln!("Token Has Been Put Up For Sale")
-                            },
-                            //some other transaction was called
-                            _ => {
-                                eprintln!("Other Transaction Called...")
-                            }
+                    //only do stuff with there is a deposit
+                    if execution_details.function_call_deposit != "" {
+                        if execution_details.receiver_id == CONTRACT_TO_VIEW {
+                            eprintln!("{} Deposited INTO Contract. Full Details --> {:?}\n\n\n\n", execution_details.function_call_deposit, execution_details);
+                        } else if execution_details.signer_id == CONTRACT_TO_VIEW || execution_details.predecessor_id == CONTRACT_TO_VIEW {
+                            eprintln!("{} SPENT By Contract. Full Details --> {:?}\n\n\n\n", execution_details.function_call_deposit, execution_details);
+                        } else {
+                            eprintln!("Not Sure What Happened --> {:?}\n\n\n\n", execution_details);
                         }
-                        //print execution details (SUCCESSFUL)
-                        eprintln!("Successful Execution Details {:?} related to Fayyr", execution_details);
+                    } else if execution_details.transfer_deposit != "" {
+                        if execution_details.receiver_id == CONTRACT_TO_VIEW {
+                            eprintln!("{} Deposited INTO Contract. Full Details --> {:?}\n\n\n\n", execution_details.transfer_deposit, execution_details);
+                        } else if execution_details.signer_id == CONTRACT_TO_VIEW || execution_details.predecessor_id == CONTRACT_TO_VIEW {
+                            eprintln!("{} SPENT By Contract. Full Details --> {:?}\n\n\n\n", execution_details.transfer_deposit, execution_details);
+                        } else {
+                            eprintln!("Not Sure What Happened --> {:?}\n\n\n\n", execution_details);
+                        }
                     } else {
-                        //print execution details (FAILED)
-                        eprintln!("Unsuccessful Execution Details {:?} related to Fayyr", execution_details);
+                        eprintln!("No Transfer Of Funds. Full Details --> {:?}\n\n\n\n", execution_details);
                     }
                 }
             }
@@ -189,8 +182,8 @@ async fn listen_blocks(mut stream: mpsc::Receiver<near_indexer::StreamerMessage>
 
 // Assuming contract deployed to account id market.test.near
 // Checks if receipt receiver is equal to the account id we care about
-fn is_fayyr_receipt(receipt: &near_indexer::near_primitives::views::ReceiptView) -> bool {
-    receipt.receiver_id.as_str() == "market2.test.near"
+fn is_contract_receipt(receipt: &near_indexer::near_primitives::views::ReceiptView) -> bool {
+    receipt.receiver_id.as_str() == CONTRACT_TO_VIEW || receipt.predecessor_id.as_str() == CONTRACT_TO_VIEW
 }
 
 fn main() {
@@ -214,9 +207,9 @@ fn main() {
             let indexer_config = near_indexer::IndexerConfig {
                 home_dir,
                 //Starts syncing from the block by which the indexer was interupted the las time it was run
-                sync_mode: near_indexer::SyncModeEnum::FromInterruption,
+                sync_mode: near_indexer::SyncModeEnum::BlockHeight(0),
                 //wait until the entire syncing process is finished before streaming starts. 
-                await_for_node_synced: near_indexer::AwaitForNodeSyncedEnum::WaitForFullSync, 
+                await_for_node_synced: near_indexer::AwaitForNodeSyncedEnum::StreamWhileSyncing, 
             };
 
             let sys = actix::System::new();
